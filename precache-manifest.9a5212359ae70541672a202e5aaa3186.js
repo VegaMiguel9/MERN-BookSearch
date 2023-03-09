b2dd06eeb689de09ce4dadddd327df27fd10eda0
Miguel Vega <vega@Miguels-MacBook-Pro.local>
2023-03-09 09:13:39 -0500

    Updates

---
+++ precache-manifest.9a5212359ae70541672a202e5aaa3186.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4e46be6b4aadb10d29a233e8d652090",
+    "url": "/Mern-BookSearch/index.html"
+  },
+  {
+    "revision": "906c316fbaacec3662d4",
+    "url": "/Mern-BookSearch/static/css/2.80f479d8.chunk.css"
+  },
+  {
+    "revision": "8110056f4f4b23d2c5dc",
+    "url": "/Mern-BookSearch/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "906c316fbaacec3662d4",
+    "url": "/Mern-BookSearch/static/js/2.e3cd4977.chunk.js"
+  },
+  {
+    "revision": "8342f58aa3e5c8f339093be2b7e8aae7",
+    "url": "/Mern-BookSearch/static/js/2.e3cd4977.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8110056f4f4b23d2c5dc",
+    "url": "/Mern-BookSearch/static/js/main.0db61e39.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/Mern-BookSearch/static/js/main.0db61e39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f39a3134681e5863d80",
+    "url": "/Mern-BookSearch/static/js/runtime-main.55b44d10.js"
+  }
+]);
